267437079ea448a469e0a2d3e2133f4a1c7b4611
Luan Henrique <luan.henrique.black@gmail.com>
2022-03-05 15:41:50 -0300

    definindo os inputs que serao limpados ao clicar no botao

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,20 @@
 jQuery(document).ready(function($y){
-    $y('.tip-percentage button').click(function(){
+    $y('.tip-percentage > *').click(function(){
         if($y(this).hasClass('active')) {
-            $y('.tip-percentage button').removeClass('active');
+            $y(this).removeClass('active');
         } else {
-            $y('.tip-percentage button').removeClass('active');
+            $y('.tip-percentage > *').removeClass('active');
             $y(this).addClass('active');
         }
+    });
+
+    const bill            = $y('#bill');
+    const numberPeople    = $y('#number-people');
+    const custom          = $y('#custom');
+
+    $y('.btn-reset').click(function(){
+        bill.val("");
+        numberPeople.val("");
+        custom.val("");
     });
 });
