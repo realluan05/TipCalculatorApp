be490eade02bddf116a8a7a92408a3ebaf46a5b1
Luan Henrique <luan.henrique.black@gmail.com>
2022-03-10 22:31:19 -0300

    calculo gorjeta por pessoa

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,6 +6,11 @@ jQuery(document).ready(function($y){
     const porcentTip      = $y('.tip-percentage > input:not(.last)');
     let buttonReset       = $y('.btn-reset');
 
+    function round(n)
+    {
+        return (Math.round(n * 100) / 100).toFixed(2);
+    }
+
     porcentTip.click(function(){
         if ($y(this).hasClass('active')) {
             $y(this).removeClass('active');
@@ -20,10 +25,20 @@ jQuery(document).ready(function($y){
         }
     });
 
+    $y('.tip-percentage > input').each(function(){
+        $y(this).focusout(function(){
+            if (bill.val() != '' && numberPeople.val() != '' && $y(this).val() != '') {
+                let tip = $y(this).val() / 100;
+                let results = (bill.val() / numberPeople.val()) * tip;
+                $y('#totals-tip-amount').text(`$ ${round(results)}`);
+            }
+        });
+    });
+
     bill.focusout(function(){
         if (bill.val() != '' && numberPeople.val() != '') {
             let results = bill.val() / numberPeople.val();
-            $y('#totals').text(`R$ ${results}`);
+            $y('#totals').text(`$ ${round(results)}`);
         }
 
         if (bill.val() != '' && numberPeople.val() != '' && porcentTip.val() != '') {
@@ -35,7 +50,7 @@ jQuery(document).ready(function($y){
     numberPeople.focusout(function(){
         if (bill.val() != '' && numberPeople.val() != '') {
             let results = bill.val() / numberPeople.val();
-            $y('#totals').text(`R$ ${results}`);
+            $y('#totals').text(`$ ${round(results)}`);
         }
 
         if (bill.val() != '' && numberPeople.val() != '' && porcentTip.val() != '') {
@@ -49,8 +64,8 @@ jQuery(document).ready(function($y){
         numberPeople.val("");
         custom.val("");
         porcentTip.removeClass('active');
-        $y('#totals-tip-amount').text("R$ 0.00");
-        $y('#totals').text("R$ 0.00");
+        $y('#totals-tip-amount').text("$ 0.00");
+        $y('#totals').text("$ 0.00");
         $y(this).attr('disabled', 'disabled');
         $y(this).addClass('disabled');
     });
