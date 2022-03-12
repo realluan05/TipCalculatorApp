ebad4012ba04835aa622eb2c17c6ac262cac271c
Luan Henrique <luan.henrique.black@gmail.com>
2022-03-11 22:07:56 -0300

    refatorando

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,18 +4,61 @@ jQuery(document).ready(function($y){
     const numberPeople    = $y('#number-people');
     const custom          = $y('#custom');
     const porcentTip      = $y('.tip-percentage > input:not(.last)');
-    let buttonReset       = $y('.btn-reset');
+    let   tipAmount       = $y('#totals-tip-amount');
+    let   totals          = $y('#totals');
+    let   buttonReset     = $y('.btn-reset');
 
     function round(n)
     {
         return (Math.round(n * 100) / 100).toFixed(2);
     }
 
+    function clear()
+    {
+        bill.val("");
+        numberPeople.val("");
+        custom.val("");
+        porcentTip.removeClass('active');
+        tipAmount.text("$0.00");
+        totals.text("$0.00");
+        $y(this).attr('disabled', 'disabled');
+        $y(this).addClass('disabled');
+    }
+
+    function percentageTip(element)
+    {
+        if (element.hasClass('active')) {
+            let tip = element.val() / 100;
+            let results = (bill.val() / numberPeople.val()) * tip;
+            tipAmount.text(`$${round(results)}`);
+        }
+    }
+
+    function updateTotals(element)
+    {
+        element.focusout(function(){
+            if (bill.val() != '' && numberPeople.val() != '') {
+                let results = bill.val() / numberPeople.val();
+                totals.text(`$${round(results)}`);
+            }
+
+            if (bill.val() != '' && numberPeople.val() != '' && porcentTip.val() != '') {
+                buttonReset.removeAttr('disabled');
+                buttonReset.removeClass('disabled');
+            }
+
+            percentageTip($y('.tip-percentage > input'));
+        });
+    }
+
+    updateTotals(bill);
+    updateTotals(numberPeople);
+
     porcentTip.click(function(){
         if ($y(this).hasClass('active')) {
             $y(this).removeClass('active');
         } else {
-            $y('.tip-percentage > input:not(.last').removeClass('active');
+            $y('.tip-percentage > input:not(.last)').removeClass('active');
             $y(this).addClass('active');
         }
 
@@ -30,43 +73,12 @@ jQuery(document).ready(function($y){
             if (bill.val() != '' && numberPeople.val() != '' && $y(this).val() != '') {
                 let tip = $y(this).val() / 100;
                 let results = (bill.val() / numberPeople.val()) * tip;
-                $y('#totals-tip-amount').text(`$${round(results)}`);
+                tipAmount.text(`$${round(results)}`);
             }
         });
     });
 
-    bill.focusout(function(){
-        if (bill.val() != '' && numberPeople.val() != '') {
-            let results = bill.val() / numberPeople.val();
-            $y('#totals').text(`$${round(results)}`);
-        }
-
-        if (bill.val() != '' && numberPeople.val() != '' && porcentTip.val() != '') {
-            buttonReset.removeAttr('disabled');
-            buttonReset.removeClass('disabled');
-        }
-    });
-
-    numberPeople.focusout(function(){
-        if (bill.val() != '' && numberPeople.val() != '') {
-            let results = bill.val() / numberPeople.val();
-            $y('#totals').text(`$${round(results)}`);
-        }
-
-        if (bill.val() != '' && numberPeople.val() != '' && porcentTip.val() != '') {
-            buttonReset.removeAttr('disabled');
-            buttonReset.removeClass('disabled');
-        }
-    });
-
     buttonReset.click(function(){
-        bill.val("");
-        numberPeople.val("");
-        custom.val("");
-        porcentTip.removeClass('active');
-        $y('#totals-tip-amount').text("$0.00");
-        $y('#totals').text("$0.00");
-        $y(this).attr('disabled', 'disabled');
-        $y(this).addClass('disabled');
+        clear();
     });
 });
